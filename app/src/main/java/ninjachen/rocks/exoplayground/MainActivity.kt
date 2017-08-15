1eeeeef67dac3ea56843b09b45bece72f233528a
Ninja <jiachenning@gmail.com>
2017-08-15 17:39:41 +0800

    Init commit

---
+++ app/src/main/java/ninjachen/rocks/exoplayground/MainActivity.kt
@@ -0,0 +1,12 @@
+package ninjachen.rocks.exoplayground
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
